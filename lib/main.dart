eb6ead567dc2d897735bcb15f5e1e75f4dda52ab
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2020-05-19 13:08:42 +0530

    Challenge 3 - Randomise both the dice using Random() method from 'math' lib using a common function

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,50 @@ class DicePage extends StatefulWidget {
   _DicePageState createState() => _DicePageState();
 }
 
+//Challenge 3 - Use void function call for Click on Dice
+class _DicePageState extends State<DicePage> {
+  var leftImageClick = 1;
+  var rightImageClick = 1;
+
+  void getRandomDice() {
+    setState(() {
+      leftImageClick = Random().nextInt(6) + 1;
+      rightImageClick = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          getRandomDice();
+        },
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset("images/dice$leftImageClick.png"),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset("images/dice$rightImageClick.png"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 //Challenge 2 - Randomly Change Both the dice
+/*
 class _DicePageState extends State<DicePage> {
   var leftImageClick = 1;
   var rightImageClick = 1;
@@ -58,6 +101,7 @@ class _DicePageState extends State<DicePage> {
     );
   }
 }
+*/
 
 //Challenge 1 - Randomly Change single dic
 /*
