9d0a860351c86e7fd35ea7a07d42235229b7ca82
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2020-05-19 12:21:46 +0530

    Use Stateful widget to make our dice responsive

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,44 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftImageClick = 5;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftImageClick = 3;
+                });
+              },
+              child: Image.asset("images/dice$leftImageClick.png"),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+              onPressed: () {},
+              child: Image.asset("images/dice1.png"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+/*
+class DicePage1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -40,3 +77,4 @@ class DicePage extends StatelessWidget {
     );
   }
 }
+*/
